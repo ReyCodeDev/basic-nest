065322fdde93c2a10a95d604492a86a5b13206cd
Adrian Rey <rey.code@proton.me>
2024-10-18 00:42:23 +0200

    Static files + template with pug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WsModule } from './websocket/ws.module';
 import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -13,6 +15,9 @@ import { AuthModule } from './auth/auth.module';
     }),
     WsModule,
     AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
