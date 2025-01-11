a2865b526f770cb55d68b7ea643de7d173cff1f9
Adrian Rey <rey.code@proton.me>
2025-01-11 16:36:51 +0100

    update

--- src/public/scripts/script.js
+++ src/public/scripts/script.js
@@ -1,7 +1,18 @@
 const socket = io();
 socket.on('onMessage', (e) => {
+  const p = document.createElement('p');
+  p.className = 'message';
+  p.innerHTML = `<p class="user">${e.user}</p><p>${e.message}</p>`;
+  document.getElementById('messages').appendChild(p);
   console.log('Received onMessage', e);
 });
-socket.emit('newMessage', {
-  message: 'Hello World!',
-});
+document.onkeydown = (e) => {
+  if (e.key === 'Enter') {
+    const textField = document.getElementById('textfield');
+    socket.emit('newMessage', {
+      message: textField.value,
+      user: 'test',
+    });
+    textField.value = '';
+  }
+};
